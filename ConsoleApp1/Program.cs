b50c938fdf1ed141d8624f048c5e92056a310586
profAlpay <alpayalbayrak59@gmail>
2024-06-24 09:59:29 +0300

    jkknkn

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
+var x = "hhh";
+x += "jjjhıuhı";
 Console.WriteLine("Hello, Alpay!");
---
+++ ConsoleApp1/anaysis.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+public class Analysis
+{
+    public void TotalPrice(string userId)
+    {
+        // CSV dosyalarını okuyun
+        var subscriptionsData = File.ReadAllLines("subscriptions.csv");
+
+        var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            HasHeaderRecord = false
+        };
+
+        List<Price> priceList;
+        using (var reader = new StreamReader("price.csv"))
+        using (var csv = new CsvReader(reader, config))
+        {
+            priceList = new List<Price>(csv.GetRecords<Price>());
+        }
+
+        // Kullanıcının aboneliklerini alın
+        List<string> userSubscriptions = new List<string>();
+        foreach (var line in subscriptionsData)
+        {
+            if (line.StartsWith(userId))
+            {
+                var parts = line.Trim().Split(',');
+                for (int i = 1; i < parts.Length; i += 2)
+                {
+                    userSubscriptions.Add(parts[i]);
+                }
+                break;
+            }
+        }
+
+        // Kullanıcının abonelik fiyatlarını toplayın
+        decimal totalCost = 0;
+        foreach (var subscription in userSubscriptions)
+        {
+            var priceRecord = priceList.Find(p => p.SubscriptionName == subscription);
+            if (priceRecord != null)
+            {
+                decimal price = priceRecord.Price;
+                Console.WriteLine(price);
+                totalCost += price;
+            }
+        }
+
+        Console.WriteLine($"user toplam abonelik ücreti: ${totalCost:F2}");
+    }
+}
+
+public class Price
+{
+    public string SubscriptionName { get; set; }
+    public decimal Price { get; set; }
+}
